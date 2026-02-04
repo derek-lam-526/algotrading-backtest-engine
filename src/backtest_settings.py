6af57b73a698c4b8b061b1a0ca84cefbe1ed1940
derek-lam-526 <dereklam050526@gmail.com>
2026-02-04 02:30:06 +0000

    chore: add batch test symbols

--- src/backtest_settings.py
+++ src/backtest_settings.py
@@ -10,27 +10,23 @@ RUN_MODE = "BATCH"
 
 # --- SYMBOL CHOICE ---
 SINGLE_SYMBOL = "DOCU"
-# BATCH_SYMBOLS = [
-#     "AAPL", "NVDA", "TSLA", "MSFT", "AMD", 
-#     "META", "AMZN", "GOOGL", "KO", "SPY"
-# ]
-
 BATCH_SYMBOLS = [
-    "QQQ", "TSM"
+    "AAPL", "NVDA", "TSLA", "MSFT", "AMD", 
+    "META", "AMZN", "GOOGL", "KO", "SPY"
 ]
 
 # --- DATES & TIMEFRAME ---
-START_DATE = datetime(2024, 12, 1)
+START_DATE = datetime(2023, 12, 1)
 END_DATE   = datetime(2026, 1, 1)
 
 TIMEFRAME  = TimeFrame(1, TimeFrameUnit.Hour) # Minute, Hour, Day, Week, Month
 
 # --- ACCOUNT SETTINGS ---
-INITIAL_CASH = 50_000
+INITIAL_CASH = 50000
 COMMISSION   = (0.35, 0.001)  # (Minimum $0.35 per trade, or 0.1% volume)
 
 # --- STRATEGY ---
-ACTIVE_STRATEGY = strategies.SmaCross
+ACTIVE_STRATEGY = strategies.LrcReversion
 
 # --- STRATEGY PARAMETERS ---
 # These override the default values inside your Strategy Class.
